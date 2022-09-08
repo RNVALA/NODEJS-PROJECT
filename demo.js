80baa1928670272ae8d0780dc40f5c52535d5b66
RNVALA <112707550+RNVALA@users.noreply.github.com>
2022-09-08 11:22:04 +0530

    V1

---
+++ demo.js
@@ -0,0 +1,21 @@
+console.log("welcomr to NODE JS");
+var a=10;
+var b=20;
+var c= a+b;
+console.log(c);
+console.log("A value is"+a+"value of b is"+b);
+console.log(`advance`);
+console.log(`a value is ${a} and   value is ${b} sum is${c}`);
+if(c==30)
+{
+    console.log("sum is 30");
+}
+else{
+    console.log("else");
+}
+var n=5;
+for(var i=0;i<n; i++)
+{
+    console.log(`hello`);
+}
+//node packge manager
